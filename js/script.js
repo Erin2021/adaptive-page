1ad0a3bb60d9e4acd27e06f8c1a4115bb4d9ec9c
김태연/Erin_Kim <76546136+Erin2021@users.noreply.github.com>
2024-06-16 18:24:28 +0900

    Update script.js
    의료진찾기 랜덤 이미지 루트 수정

--- js/script.js
+++ js/script.js
@@ -65,7 +65,7 @@ const images =["0.jpg","1.jpg","2.jpg","3.jpg","4.jpg"];
 const chosenImage =images[Math.floor(Math.random()*images.length)];
 
 const bgImage= document.querySelector("#section2 .findDoc");
-bgImage.style.background=`url(../img/${chosenImage}) no-repeat right top / cover`;
+bgImage.style.background=`url(img/${chosenImage}) no-repeat right top / cover`;
 
 
 
